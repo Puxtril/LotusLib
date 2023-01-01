e8ecdf42951c73577aa945a086089afd1fb93a77
Puxtril <Puxtril@protonmail.com>
2023-01-01 17:02:15 -0100

    Replaced PackageCollection iterator with a vector

--- include/PackageCollection.h
+++ include/PackageCollection.h
@@ -4,6 +4,7 @@
 
 #include <filesystem>
 #include <unordered_map>
+#include <vector>
 #include <string>
 
 namespace LotusLib
@@ -19,7 +20,8 @@ namespace LotusLib
 	{
 		std::filesystem::path m_packageDir;
 		bool m_isPostEnsmallening;
-		std::unordered_map<std::string, Package<T>> m_pkgs;
+		std::unordered_map<std::string, int> m_pkgMap;
+		std::vector<Package<T>> m_pkgs;
 		Logger& m_log;
 
 	public:
@@ -32,22 +34,24 @@ namespace LotusLib
 
 		PackageCollection(const PackageCollection&) = delete;
 		PackageCollection& operator=(PackageCollection&) = delete;
+		PackageCollection(PackageCollection&&) = default;
+		PackageCollection& operator=(PackageCollection&&) = default;
 
-		typename std::unordered_map<std::string, Package<T>>::iterator begin() { return m_pkgs.begin(); }
-		typename std::unordered_map<std::string, Package<T>>::iterator end() { return m_pkgs.end(); }
-		typename std::unordered_map<std::string, Package<T>>::const_iterator begin() const { return m_pkgs.begin(); }
-		typename std::unordered_map<std::string, Package<T>>::const_iterator end() const { return m_pkgs.end(); }
+		typename std::vector<Package<T>>::iterator begin() { return m_pkgs.begin(); }
+		typename std::vector<Package<T>>::iterator end() { return m_pkgs.end(); }
+		typename std::vector<Package<T>>::const_iterator begin() const { return m_pkgs.begin(); }
+		typename std::vector<Package<T>>::const_iterator end() const { return m_pkgs.end(); }
 
 		const Package<T>&
 		operator[](const std::string& pkgName) const
 		{
-			return m_pkgs.at(pkgName);
+			return m_pkgs[m_pkgMap.at(pkgName)];
 		}
 
 		Package<T>&
 		operator[](const std::string& pkgName)
 		{
-			return m_pkgs.at(pkgName);
+			return m_pkgs[m_pkgMap.at(pkgName)];
 		}
 
 		const std::filesystem::path& getPath() const { return m_packageDir; }
@@ -66,13 +70,13 @@ namespace LotusLib
 					continue;
 		
 				std::string pkgName = filename.substr(start + 1, end - 2);
-				if (m_pkgs.find(pkgName) == m_pkgs.end())
+				if (m_pkgMap.find(pkgName) == m_pkgMap.end())
 					_loadPackage(pkgName);
 			}
 		
 			std::string pkgList = "";
 			for (const auto& x : m_pkgs)
-				pkgList += x.first + " ";
+				pkgList += x.getName() + " ";
 			m_log.info(spdlog::fmt_lib::format("Loaded {} packages: {}", m_pkgs.size(), pkgList));
 		}
 
@@ -80,11 +84,12 @@ namespace LotusLib
 		{
 			// Because Package does not have a copy constructor
 			// https://stackoverflow.com/a/27699086
-			m_pkgs.emplace(
+			m_pkgMap.emplace(
 				std::piecewise_construct,
 				std::forward_as_tuple(pkgName),
-				std::forward_as_tuple(m_packageDir, pkgName, m_isPostEnsmallening)
+				std::forward_as_tuple(m_pkgs.size())
 			);
+			m_pkgs.emplace_back(m_packageDir, pkgName, m_isPostEnsmallening);
 		} 
 	};
 }
