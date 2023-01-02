c3f04eb4ce6bc0649efde26a10376dee9d03d81a
Puxtril <Puxtril@protonmail.com>
2023-01-02 21:48:15 -0100

    Not implementing this publically

--- src/CachePairWriter.cpp
+++
@@ -1,8 +0,0 @@
-#include "CachePairWriter.h"
-
-using namespace LotusLib;
-
-CachePairWriter::CachePairWriter(std::filesystem::path tocPath, std::filesystem::path cachePath, bool isPostEnsmallening)
-	: CachePair(tocPath, cachePath, isPostEnsmallening)
-{
-}
