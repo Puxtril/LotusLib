4fd7d2e712cc26b17b06c5c6c4fe806718d6f369
Puxtril <puxtril@protonmail.com>
2024-05-02 20:11:14 -0400

    Removed silly LotusPath constraints

--- include/LotusPath.h
+++ include/LotusPath.h
@@ -6,80 +6,8 @@
 
 namespace LotusLib
 {
-	// Constraints:
-	//  * Path must be unix-like (/ delimiter)
-	//  * Path must be absolute
-	class LotusPath : public std::filesystem::path
-	{
-		void checkSeparator()
-		{
-			if (this->string().find('\\') != std::string::npos)
-				throw LotusLib::InvalidLotusPath("LotusPath contains invalid separator: " + this->string());
-		}
-
-	public:
-		using value_type = char;
-		static constexpr value_type preferred_separator = '/';
-
-		LotusPath() = default;		
-
-		LotusPath(const std::filesystem::path& p)
-			: std::filesystem::path(p)
-		{
-			checkSeparator();
-		}
-
-		LotusPath(std::filesystem::path&& p) noexcept
-			: std::filesystem::path(p)
-		{
-			checkSeparator();
-		}
-
-		LotusPath(string_type&& source, format fmt = auto_format)
-			: std::filesystem::path(std::move(source), fmt)
-		{
-			checkSeparator();
-		}
-
-		template< class Source>
-		LotusPath(const Source& source, format fmt = auto_format)
-			: std::filesystem::path(source, fmt)
-		{
-			checkSeparator();
-		}
-
-		template< class InputIt >
-		LotusPath(InputIt first, InputIt last, format fmt = auto_format)
-			: std::filesystem::path(first, last, fmt)
-		{
-			checkSeparator();
-		}
-
-		template< class Source >
-		LotusPath(const Source& source, const std::locale& loc, format fmt = auto_format)
-			: std::filesystem::path(source, loc, fmt)
-		{
-			checkSeparator();
-		}
-
-		template< class InputIt >
-		LotusPath(InputIt first, InputIt last, const std::locale& loc, format fmt = auto_format)
-			: std::filesystem::path(first, last, loc, fmt)
-		{
-			checkSeparator();
-		}
-
-		std::filesystem::path& make_preferred() noexcept
-		{
-			return *this;
-		}
-
-		std::filesystem::path
-		getPreferredPath() const
-		{
-			std::filesystem::path newPath = *this;
-			newPath.make_preferred();
-			return newPath;
-		}
-	};
+	// Statically constraining this is unecessary
+	// Just make this a forward-slash path
+	// ex. /Lotus/Characters/Tenno/Excalibur/ExcaliburBody_skel.fbx
+	using LotusPath = std::filesystem::path;
 }
