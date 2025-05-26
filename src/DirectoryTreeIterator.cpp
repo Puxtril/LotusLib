Puxtril/LotusLib

commit 71eebedb7db3faefc5163777e6c9fed9a3e3a321

Iterator edge case fix
This patches an edge case where the starting directory contains no valid
files. The directory likely only exists because deleted files exist
somewhere in the tree.

src/DirectoryTreeIterator.cpp
@@ -10,7 +10,7 @@ DirectoryTreeIterator::DirectoryTreeIterator(const FileEntries::DirNode* ptr)
     : m_startingPos(ptr), m_ptr(ptr), m_dirPositions(std::stack<uint32_t>()), m_filePosition(0)
 {
     m_dirPositions.push(0);
-    findNextValidDir();
+    ++(*this);
 }
 
 DirectoryTreeIterator&

commit 67f5827111f59f0efd84996821c6e2603af5ec95

Alternative fix for 71eebed
71eebed introduced another bug - this iterator skips the first file in
the specified directory. This fix patches both bugs.

src/DirectoryTreeIterator.cpp
@@ -10,7 +10,13 @@ DirectoryTreeIterator::DirectoryTreeIterator(const FileEntries::DirNode* ptr)
     : m_startingPos(ptr), m_ptr(ptr), m_dirPositions(std::stack<uint32_t>()), m_filePosition(0)
 {
     m_dirPositions.push(0);
-    ++(*this);
+    
+    bool atEnd = findNextValidDir();
+    if (atEnd)
+    {
+        m_ptr = nullptr;
+        m_filePosition = 0;
+    }
 }
 
 DirectoryTreeIterator&